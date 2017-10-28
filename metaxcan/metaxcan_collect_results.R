3bb3ab0615fef16ec790945c9863dc741cfb9540
Demis Kia <dkia@ion.ucl.ac.uk>
2017-10-28 19:20:47 +0100

    twas & metaxcan

---
+++ metaxcan/metaxcan_collect_results.R
@@ -0,0 +1,38 @@
+library(stringr)
+setwd("metaxcan_se_results/")
+
+tests <- list.files()
+for (i in 1:length(tests)){
+  assign(tests[i], read.table(tests[i], sep=",", header=TRUE, stringsAsFactors=FALSE))
+}
+results <- setNames(lapply(tests, function(x) get(x)), tests)
+
+for(j in 1:length(results))
+{
+  temp <- results[[j]]
+  temp$Pvalue <- as.numeric(as.character(temp$pvalue))
+  data <- subset(temp, !is.na(temp$Pvalue))
+  data$Pvalue_FDR <- p.adjust(data$Pvalue, method = c("fdr"))
+  data$Pvalue_bonferroni <- p.adjust(data$Pvalue, method = c("bonferroni"))
+  dat <- subset(data, Pvalue_FDR <= 0.05)
+  dat <- dat[order(dat$Pvalue),]
+  dat$Region <- names(results)[j]
+  dat$Region <- str_replace_all(dat$Region, "_0.5_1KG_results_se.txt", "")
+  dat$Region <- str_replace_all(dat$Region, "TW_", "")
+  write.table(dat, file = paste(names(results)[j],"_FDR_HITS.tsv",sep = ""), quote = F, sep = "\t", row.names = F, col.names=TRUE)
+}
+
+
+fdrs <- list.files(pattern = "FDR_HITS")
+for (i in 1:length(fdrs)){
+  assign(fdrs[i], read.table(fdrs[i], sep="\t", header=TRUE, stringsAsFactors=FALSE))
+}
+fdr_results <- setNames(lapply(fdrs, function(x) get(x)), fdrs)
+
+full_fdr_results <- NULL
+for(j in 1:length(fdr_results))
+{
+  full_fdr_results <- rbind(full_fdr_results, fdr_results[[j]])
+}
+full_fdr_results <- full_fdr_results[order(full_fdr_results$pvalue),]
+write.table(full_fdr_results, file = "metaxcan_merged_fdr_results.tab", quote = FALSE, sep = "\t", row.names = FALSE, col.names=TRUE)
